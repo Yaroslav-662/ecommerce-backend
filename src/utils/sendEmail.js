72a4ed9e908a3b8689b3d48cf24aa52393719e9b
Yaroslav-662 <paroslav662@gmail.com>
2026-05-18 13:57:12 +0300

    Update email transport settings to use SSL

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -6,8 +6,8 @@ export async function sendEmail({ to, subject, text, html }) {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
